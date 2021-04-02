5b363a3cb92c2a0224954fb00b3ce4efde0951e9
layn-cyber <78388844+layn-cyber@users.noreply.github.com>
2021-04-02 19:28:50 +0700

    Add files via upload

---
+++ se.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<title>Hacked by L4yn.ID  !</title>
+
+<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Just+Another+Hand" rel="stylesheet"> 
+<link rel="icon" type="image/png" href="https://e.top4top.io/p_1802k0kyl1.jpeg"/>
+<center>
+<style>
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
+.x{
+    animation: shake 1s;
+  animation-iteration-count: infinite;
+  font-family: 'Just Another Hand', cursive;
+  font-size:58px;
+}
+.z{
+  font-family: 'PT Sans Narrow', cursive;
+  font-size:20px;
+}
+</style>
+<body bgcolor='black'><font color='white'><pre>
+
+</pre>
+<div class=x>Hacked by L4yn.ID  &#x1F494;</div><br>
+<p>gmakarim7@gmail.com<p>
+<div class=z>
+<br><br><br><br><font face="'Patrick Hand', cursive" color="white">Tangerang Xploit Team - Isekai Xploit  - Tangerang Cyber Army - Wibu Cyber Team - PhantomSec1337</font></marquee> </a> </div> <center><iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://l.top4top.io/m_1633too220.mp3 " frameborder="0" allowfullscreen></iframe> </center> <style> body { background: url("https://www.gambaranimasi.org/data/media/687/animasi-bergerak-tengkorak-0097.gif ") no-repeat fixed; background-size: 100% 100%; background-color: black; color: white; } } </style> <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+    
+Cheers:<br>
+IsekaiXploit - TangerangXploit Team - Wibu Cyber Team - Phantomsec1337 - HexsploitTeam
+</div> <br>
+ Thanks To Friend : Alf404 - mrd30 - ./MRF4J4R - YIIX103 - Dannu - VenoRyan - hasbyx12
